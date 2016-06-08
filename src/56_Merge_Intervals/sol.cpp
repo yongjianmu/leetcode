ec4ec270d7749f7eb7852b1ea6e796d56a95f84e
Yongjian Mu <Yongjian@YongjiandeMacBook-Pro.local>
2016-06-08 03:16:30 -0600

    Add 56

--- src/56_Merge_Intervals/sol.cpp
+++ src/56_Merge_Intervals/sol.cpp
@@ -11,80 +11,85 @@ using namespace std;
    */
 
 struct Interval {
-	int start;
-	int end;
-	Interval() : start(0), end(0) {}
-	Interval(int s, int e) : start(s), end(e) {}
+    int start;
+    int end;
+    Interval() : start(0), end(0) {}
+    Interval(int s, int e) : start(s), end(e) {}
 };
 
 class Solution {
-	public:
-		vector<Interval> merge(vector<Interval>& intervals) {
-			vector<Interval> ret;
-			int size = intervals.size();
-			if(0 == size)
-			{
-				return ret;
-			}
+    public:
+        vector<Interval> merge(vector<Interval>& intervals) {
+            vector<Interval> ret;
+            int size = intervals.size();
+            if(0 == size)
+            {
+                return ret;
+            }
 
-			// Merge items which has the same first value
-			for(int i = 0; i < size; ++i)
-			{
-				
-			}
+            // Merge items which has the same first value
+            unordered_map<int, int> dict;
+            vector<int> key_list;
+            for(int i = 0; i < size; ++i)
+            {
+                if(dict.find(intervals[i].start) == dict.end())
+                {
+                    dict[intervals[i].start] = intervals[i].end;
+                    key_list.push_back(intervals[i].start);
+                }
+                else
+                {
+                    if(dict[intervals[i].start] < intervals[i].end)
+                    {
+                        dict[intervals[i].start] = intervals[i].end;
+                    }
+                }
+            }
 
-			unordered_map<int, int> dict;
-			vector<int> key_list;
-			for(int i = 0; i < size; ++i)
-			{
-				dict[intervals[i].start] = intervals[i].end;
-				key_list.push_back(intervals[i].start);
-			}
+            sort(key_list.begin(), key_list.end());
 
-			sort(key_list.begin(), key_list.end());
+            Interval curr(key_list[0], dict[key_list[0]]);
+            for(int i = 1; i < size; ++i)
+            {
+                if(curr.end >= key_list[i])
+                {
+                    if(curr.end < dict[key_list[i]])
+                    {
+                        curr.end = dict[key_list[i]];
+                    }
+                }
+                else
+                {
+                    ret.push_back(curr);
+                    curr.start = key_list[i];
+                    curr.end = dict[key_list[i]];
+                }
+            }
+            ret.push_back(curr);
 
-			Interval curr(key_list[0], dict[key_list[0]]);
-			for(int i = 1; i < size; ++i)
-			{
-				if(curr.end >= key_list[i])
-				{
-					if(curr.end < dict[key_list[i]])
-					{
-						curr.end = dict[key_list[i]];
-					}
-				}
-				else
-				{
-					ret.push_back(curr);
-					curr.start = key_list[i];
-					curr.end = dict[key_list[i]];
-				}
-			}
-			ret.push_back(curr);
-
-			return ret;
-		}
+            return ret;
+        }
 };
 
 int main()
 {
-	Interval i1(1, 3);
-	Interval i2(2, 6);
-	Interval i3(8, 10);
-	Interval i4(15, 18);
+    Interval i1(1, 4);
+    Interval i2(1, 5);
+    Interval i3(8, 10);
+    Interval i4(15, 18);
 
-	vector<Interval> input;
-	input.push_back(i1);
-	input.push_back(i2);
-	input.push_back(i3);
-	input.push_back(i4);
+    vector<Interval> input;
+    input.push_back(i1);
+    input.push_back(i2);
+    //input.push_back(i3);
+    //input.push_back(i4);
 
-	Solution sol;
-	vector<Interval> result = sol.merge(input);
-	for(int i = 0; i < result.size(); ++i)
-	{
-		cout << result[i].start << ", " << result[i].end << endl;
-	}
+    Solution sol;
+    vector<Interval> result = sol.merge(input);
+    for(int i = 0; i < result.size(); ++i)
+    {
+        cout << result[i].start << ", " << result[i].end << endl;
+    }
 
-	return 0;
+    return 0;
 }
