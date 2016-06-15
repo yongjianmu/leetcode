913ee5c46d3cb71d996a1e780f582f2a5fc1b413
Yongjian Mu <Yongjian@YongjiandeMacBook-Pro.local>
2016-06-15 01:55:33 -0600

    Add 75 76

--- src/74_Search_a_2D_Matrix/sol.cpp
+++ src/74_Search_a_2D_Matrix/sol.cpp
@@ -4,100 +4,100 @@
 using namespace std;
 
 /*
-[
-  [1,   3,  5,  7],
-  [10, 11, 16, 20],
-  [23, 30, 34, 50]
-]
-*/
+   [
+   [1,   3,  5,  7],
+   [10, 11, 16, 20],
+   [23, 30, 34, 50]
+   ]
+   */
 
 class Solution {
-public:
-    bool searchMatrix(vector<vector<int> >& matrix, int target) {
-        int row = matrix.size();
-        if(0 == row)
-        {
-            return false;
-        }
-
-        int col = matrix[0].size();
-        if(0 == col)
-        {
-            return false;
-        }
-
-        if(matrix[0][0] > target || matrix[row - 1][col - 1] < target)
-        {
-            return false;
-        }
-
-        int rowL = 0, rowR = row - 1;
-        int retRow = 0;
-        while(rowL <= rowR)
-        {
-            int mid = (rowL + rowR) >> 1;
-            if(matrix[mid][col - 1] < target)
+    public:
+        bool searchMatrix(vector<vector<int> >& matrix, int target) {
+            int row = matrix.size();
+            if(0 == row)
             {
-                if(row - 1 == mid)
-                {
-                    retRow = mid;
-                    break;
-                }
+                return false;
+            }
 
-                if(matrix[mid + 1][col - 1] >= target)
+            int col = matrix[0].size();
+            if(0 == col)
+            {
+                return false;
+            }
+
+            if(matrix[0][0] > target || matrix[row - 1][col - 1] < target)
+            {
+                return false;
+            }
+
+            int rowL = 0, rowR = row - 1;
+            int retRow = 0;
+            while(rowL <= rowR)
+            {
+                int mid = (rowL + rowR) >> 1;
+                if(matrix[mid][col - 1] < target)
                 {
-                    retRow = mid + 1;
-                    break;
+                    if(row - 1 == mid)
+                    {
+                        retRow = mid;
+                        break;
+                    }
+
+                    if(matrix[mid + 1][col - 1] >= target)
+                    {
+                        retRow = mid + 1;
+                        break;
+                    }
+                    else
+                    {
+                        rowL = mid + 1;
+                    }
+                }
+                else if(matrix[mid][col - 1] > target)
+                {
+                    if(0 == mid || matrix[mid][0] <= target)
+                    {
+                        retRow = mid;
+                        break;
+                    }
+
+                    if(matrix[mid - 1][col - 1] <= target)
+                    {
+                        retRow = mid - 1;
+                        break;
+                    }
+                    else
+                    {
+                        rowR = mid - 1;
+                    }
                 }
                 else
                 {
-                    rowL = mid + 1;
+                    return true;
                 }
             }
-            else if(matrix[mid][col - 1] > target)
-            {
-                if(0 == mid || matrix[mid][0] <= target)
-                {
-                    retRow = mid;
-                    break;
-                }
 
-                if(matrix[mid - 1][col - 1] <= target)
+            int colL = 0, colR = col - 1;
+            while(colL <= colR)
+            {
+                int mid = (colL + colR) >> 1;
+                if(matrix[retRow][mid] < target)
                 {
-                    retRow = mid - 1;
-                    break;
+                    colL = mid + 1;
+                }
+                else if(matrix[retRow][mid] > target)
+                {
+                    colR = mid - 1;
                 }
                 else
                 {
-                    rowR = mid - 1;
+                    return true;
                 }
             }
-            else
-            {
-                return true;
-            }
-        }
 
-        int colL = 0, colR = col - 1;
-        while(colL <= colR)
-        {
-            int mid = (colL + colR) >> 1;
-            if(matrix[retRow][mid] < target)
-            {
-                colL = mid + 1;
-            }
-            else if(matrix[retRow][mid] > target)
-            {
-                colR = mid - 1;
-            }
-            else
-            {
-                return true;
-            }
+            return false;
         }
-
-        return false;
-    }
 };
 
 int main()
