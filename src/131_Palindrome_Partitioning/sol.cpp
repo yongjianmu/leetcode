b30aa355f3f860c8a54fdedafa89ce5247abd0de
Yongjian Mu <Yongjian@YongjiandeMacBook-Pro.local>
2016-06-25 02:35:38 -0600

    Update 131

--- src/131_Palindrome_Partitioning/sol.cpp
+++ src/131_Palindrome_Partitioning/sol.cpp
@@ -11,80 +11,80 @@
 using namespace std;
 
 /*
-For example, given s = "aab",
-Return
+   For example, given s = "aab",
+   Return
 
-[
-  ["aa","b"],
-  ["a","a","b"]
-]
-*/
+   [
+   ["aa","b"],
+   ["a","a","b"]
+   ]
+   */
 
 class Solution {
 public:
-	bool isValid(string str)
-	{
-		int len = str.length();
-		if(1 >= len)
-		{
-			return true;
-		}
+    bool isValid(string str)
+    {
+        int len = str.length();
+        if(1 >= len)
+        {
+            return true;
+        }
 
-		int start = 0;
-		int end = len - 1;
-		while(start <= end)
-		{
-			if(str[start++] != str[end--])
-			{
-				return false;
-			}
-		}
-		return true;
-	}
+        int start = 0;
+        int end = len - 1;
+        while(start <= end)
+        {
+            if(str[start++] != str[end--])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
 
-	void solve(vector<string> cur, int start, int end, string s, vector<vector<string> >& ret)
-	{
-		if(start >= end)
-		{
-			ret.push_back(cur);
-			return;
-		}
+    void solve(vector<string> cur, int start, int end, string s, vector<vector<string> >& ret)
+    {
+        if(start >= end)
+        {
+            ret.push_back(cur);
+            return;
+        }
 
-		for(int i = start; i < end; ++i)
-		{
-			string str = s.substr(start, i + 1);
-			if(isValid(str))
-			{
-				vector<string> vec;
-				vec.insert(vec.begin(), cur.begin(), cur.end());
-				vec.push_back(str);
-				solve(vec, i + 1, end, s, ret);
-			}
-		}
-	}
+        for(int i = start; i < end; ++i)
+        {
+            string str = s.substr(start, i + 1 - start);
+            if(isValid(str))
+            {
+                vector<string> vec;
+                vec.insert(vec.begin(), cur.begin(), cur.end());
+                vec.push_back(str);
+                solve(vec, i + 1, end, s, ret);
+            }
+        }
+    }
 
     vector<vector<string> > partition(string s) {
-    	vector<vector<string> > ret;
-    	vector<string> cur;
-    	solve(cur, 0, s.length(), s, ret);
-    	return ret;
+        vector<vector<string> > ret;
+        vector<string> cur;
+        solve(cur, 0, s.length(), s, ret);
+        return ret;
     }
 };
 
 int main()
 {
-	string s = "aab";
-	Solution sol;
-	vector<vector<string> > result = sol.partition(s);
-	for(int i = 0; i < result.size(); ++i)
-	{
-		for(int j = 0; j < result[i].size(); ++j)
-		{
-			cout << result[i][j] << ", ";
-		}
-		cout << endl;
-	}
-	return 0;
+    string s = "aab";
+    Solution sol;
+    vector<vector<string> > result = sol.partition(s);
+    for(int i = 0; i < result.size(); ++i)
+    {
+        for(int j = 0; j < result[i].size(); ++j)
+        {
+            cout << result[i][j] << ", ";
+        }
+        cout << endl;
+    }
+    return 0;
 }
 
 
