f6c5575f49f6698a46e7db585706752f2694c10f
Yongjian Mu <Yongjian@YongjiandeMacBook-Pro.local>
2016-07-14 22:34:51 -0600

    Update 17

--- second/17_Letter_Combinations_of_a_Phone_Number/main.cpp
+++ second/17_Letter_Combinations_of_a_Phone_Number/main.cpp
@@ -8,25 +8,42 @@ Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
 class Solution {
 public:
     vector<string> mRet;
+    int len;
 
     void solve(int pos, string cur, string digits, vector<vector<char> > dict)
     {
+        if(pos >= len)
+        {
+            mRet.push_back(cur);
+            return;
+        }
 
+        for(int i = 0; i < dict[digits[pos] - '0'].size(); ++i)
+        {
+            cur.push_back(dict[digits[pos] - '0'][i]);
+            solve(pos + 1, cur, digits, dict);
+            cur.pop_back();
+        }
     }
 
-    vector<string> letterCombinations(string digits) {
-        int len = digits.length();
+    vector <string> letterCombinations(string digits) {
+        len = digits.length();
+        if(0 == len)
+        {
+            return mRet;
+        }
+
         vector<vector<char> > dict = {
-            {' '},
-            {''},
-            {'a', 'b', 'c'},
-            {'d', 'e', 'f'},
-            {'g', 'h', 'i'},
-            {'j', 'k', 'l'},
-            {'m', 'n', 'o'},
-            {'p', 'q', 'r', 's'},
-            {'t', 'u', 'v'},
-            {'w', 'x', 'y', 'z'}
+                {' '},
+                {},
+                {'a', 'b', 'c'},
+                {'d', 'e', 'f'},
+                {'g', 'h', 'i'},
+                {'j', 'k', 'l'},
+                {'m', 'n', 'o'},
+                {'p', 'q', 'r', 's'},
+                {'t', 'u', 'v'},
+                {'w', 'x', 'y', 'z'}
         };
 
         string cur;
@@ -35,7 +52,17 @@ public:
     }
 };
 
-int main() {
-    cout << "Hello, World!" << endl;
+int main()
+{
+    string input = "23";
+    Solution sol;
+    vector<string> result = sol.letterCombinations(input);
+    cout << "#### Result ####" << endl;
+    for(int i = 0; i < result.size(); ++i)
+    {
+        cout << result[i] << ", ";
+    }
+    cout << endl;
+
     return 0;
 }
