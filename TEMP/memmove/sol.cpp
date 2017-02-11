dc964b5e974c03829804909c0c8c3dfe87c115be
James Mu <James@yongjiandembp.uconnect.utah.edu>
2017-02-10 20:43:26 -0700

    udpate

--- TEMP/memmove/sol.cpp
+++ TEMP/memmove/sol.cpp
@@ -1,41 +1,21 @@
-
 void* memmove(void* dest, void* source, size_t count)
-
 {
-
     void* ret = dest;
-
-
     if (dest <= source || dest >= (source + count))
-
     {
-
         //Non-Overlapping Buffers
         //copy from lower addresses to higher addresses
-
         while (count --)
-
           *dest++ = *source++;
-
     }
-
     else
-
     {
-
         //Overlapping Buffers
         //copy from higher addresses to lower addresses 内存重叠的情况
-
         dest += count - 1;
-
         source += count - 1;
-
         while (count--)
-
           *dest-- = *source--;
-
     }
-
     return ret;
-
 }
