c5931c484742800c80c2bd0b36dffdaa675a8e00
James Mu <James@yongjiandembp.uconnect.utah.edu>
2017-02-11 23:29:25 -0700

    update

--- TEMP/drawCircle/sol.cpp
+++ TEMP/drawCircle/sol.cpp
@@ -34,28 +34,41 @@ vector<pair<int, int> > drawCircle1(int r)
 
 vector<pair<int, int> > drawCircle2(int r)
 {
-    set<pair<int, int> > st;
+    //set<pair<int, int> > st;
     unordered_set<string> vis;
     vector<pair<int, int> > ret;
     queue<pair<int, int> > q;
     int x = 0, y = r, r2 = r * r;
     q.push({x, y});
     vis.insert(to_string(x) + "#" + to_string(y));
+    vis.insert(to_string(y) + "#" + to_string(x));
     while(!q.empty())
     {
         pair<int, int> cur = q.front(); q.pop();
-        cout << cur.first << ", " << cur.second << endl;
+        //cout << cur.first << ", " << cur.second << endl;
         int pro = cur.first * cur.first + cur.second * cur.second;
         if(pro == r2)
         {
-            st.insert(cur);
-            st.insert({cur.first, -cur.second});
-            st.insert({-cur.first, cur.second});
-            st.insert({-cur.first, -cur.second});
-            st.insert({cur.second, cur.first});
-            st.insert({cur.second, -cur.first});
-            st.insert({-cur.second, cur.first});
-            st.insert({-cur.second, -cur.first});
+            ret.push_back(cur);
+            ret.push_back({cur.second, cur.first});
+            if(cur.second != 0)
+            {
+                ret.push_back({cur.first, -cur.second});
+                ret.push_back({-cur.second, cur.first});
+            }
+
+            if(cur.first != 0)
+            {
+                ret.push_back({cur.second, -cur.first});
+                ret.push_back({-cur.first, cur.second});
+            }
+
+            if(cur.first != 0 && cur.second != 0)
+            {
+                ret.push_back({-cur.second, cur.first});
+                ret.push_back({-cur.second, -cur.first});
+            }
+
             if((cur.first + 1 < r) && (cur.first + 1 <= cur.second) && (vis.find(to_string(cur.first + 1) + "#" + to_string(cur.second)) == vis.end()))
             {
                 q.push({cur.first + 1, cur.second});
@@ -79,17 +92,17 @@ vector<pair<int, int> > drawCircle2(int r)
         }
     }
 
-    for(auto iter = st.begin(); iter != st.end(); ++iter)
-    {
-        ret.push_back({iter->first, iter->second});
-    }
+    //for(auto iter = st.begin(); iter != st.end(); ++iter)
+    //{
+    //    ret.push_back({iter->first, iter->second});
+    //}
     return ret;
 }
 
 int main()
 {
     cout << "Method1" << endl;
-    vector<pair<int, int> > result1 = drawCircle1(5);
+    vector<pair<int, int> > result1 = drawCircle1(10);
     printResult();
     for(auto& r : result1)
     {
@@ -97,7 +110,7 @@ int main()
     }
 
     cout << "Method2" << endl;
-    vector<pair<int, int> > result2 = drawCircle2(5);
+    vector<pair<int, int> > result2 = drawCircle2(10);
     printResult();
     for(auto& r : result2)
     {
